499e2a0a99df11f40b73228ae6022d46a0e15a52
Kyle Leong <k.leong@outlook.com>
2020-03-06 22:13:41 -0800

    Basic Object Moves With Camera

--- examples/project.js
+++ examples/project.js
@@ -5,6 +5,137 @@ const { Triangle, Square, Tetrahedron, Windmill, Cube, Subdivision_Sphere, Cappe
 // Pull these names into this module's scope for convenience:
 const { vec3, vec4, vec, color, Mat4, Light, Shape, Material, Shader, Texture, Scene } = tiny;
 
+let g_dx = 0, g_dy = 0;
+let g_origin_offset = vec3(0, 0, 0);
+let g_world_objects = [];
+let g_cam_looking_at = vec3(NaN, NaN, NaN);
+let g_x_ccs = vec3(NaN, NaN, NaN);
+let g_z_ccs = vec3(NaN, NaN, NaN);
+let g_z_rot = Math.PI;
+
+const FPS_Controls =
+class FPS_Controls extends defs.Movement_Controls
+{
+  constructor()
+  {
+    super();
+  }
+
+  add_mouse_controls(canvas)
+  {
+    this.mouse = { "from_center": vec( 0,0 ) };
+    const mouse_position = (e, rect = canvas.getBoundingClientRect()) =>
+        vec( e.clientX - (rect.left + rect.right)/2, e.clientY - (rect.bottom + rect.top)/2 );
+    document.addEventListener( "mouseup",   e => { this.mouse.anchor = undefined; } );
+    canvas.addEventListener( "mousedown", e => { e.preventDefault(); this.mouse.anchor = mouse_position(e); } );
+    canvas.addEventListener( "mousemove", e => { e.preventDefault(); this.mouse.from_center = mouse_position(e); } );
+    canvas.addEventListener( "mouseout",  e => { if( !this.mouse.anchor ) this.mouse.from_center.scale_by(0) } );
+
+    document.exitPointerLock = document.exitPointerLock;
+
+    canvas.onclick = () => canvas.requestPointerLock();
+
+    let updatePosition = (e) => {
+      g_dx = e.movementX;
+      g_dy = e.movementY;
+    };
+
+    let lockChangeAlert = () => {
+      if (document.pointerLockElement === canvas) {
+        document.addEventListener("mousemove", updatePosition, false);
+      } else {
+        document.removeEventListener("mousemove", updatePosition, false);
+        g_dx = g_dy = 0;
+      }
+    };
+
+    document.addEventListener('pointerlockchange', lockChangeAlert, false);
+  }
+
+  // This function is called when the user executes the movement keys, e.g. WASD.
+  first_person_flyaround(radians_per_frame, meters_per_frame, leeway = 70 )
+  {
+    // We do not want the user to move up/down i.e. along the y axis. So we do not
+    // accommodate for this.thrust[1] which is the axis.
+    // The thrust values are subtracted from the g_origin_offset because we want the
+    // objects to do the opposite of what I'm doing so it looks as if the cam is moving.
+    if (this.thrust[0] !== 0) {
+      g_origin_offset[0] -= this.thrust[0] * .1;
+    } else if (this.thrust[2] !== 0) {
+      g_origin_offset[2] -= this.thrust[2] * .1;
+    }
+  }
+
+  // This function is called whenever the mouse is moved.
+  third_person_arcball(radians_per_frame)
+  {
+    // The rotation that we do depends ONLY on the mouse dx and dy, stored in the global variables.
+    let dragging_vector = vec(g_dx, g_dy).times(40);
+    // Reset the deltas so we don't keep rotating if the pointer unlocks.
+    g_dx = g_dy = 0;
+
+    if( dragging_vector.norm() <= 0 )
+      return;
+
+    // Rotate around the y axis, i.e. horizontal movement.
+    let horiz_rot;
+    if (dragging_vector[0] !== 0) {
+      let y_ccs = this.matrix().times(vec4(0, 1, 0, 0)).to3();
+      let rot_angle = radians_per_frame * dragging_vector.norm() * (dragging_vector[0] > 0 ? 1 : -1);
+      // console.log(`Y Axis in CCS: (${y_ccs[0].toFixed(2)}, ${y_ccs[1].toFixed(2)}, ${y_ccs[2].toFixed(2)})`);
+      horiz_rot = Mat4.rotation(rot_angle, y_ccs[0], y_ccs[1], y_ccs[2]);
+    }
+
+    // Report the x and z axis w.r.t. camera coordinate system.
+    g_x_ccs = this.inverse().times(vec4(1, 0, 0, 0)).to3();
+    g_z_ccs = this.inverse().times(vec4(0, 0, 1, 0)).to3();
+
+    if (horiz_rot) {
+      this.matrix().post_multiply(horiz_rot);
+      this.inverse().pre_multiply(horiz_rot);
+    }
+
+    console.log(`CamZ: (${this.matrix()[0][2].toFixed(2)},
+    ${this.matrix()[1][2].toFixed(2)},
+    ${this.matrix()[2][2].toFixed(2)})`);
+
+    // Change sign of z component because we are looking down the negative z axis.
+    let cam = this.inverse();
+    g_cam_looking_at = vec3(cam[0][2], cam[1][2], cam[2][2] * -1);
+
+    // Compute angle of rotation between z axis and what I'm looking at.
+    // g_z_rot = Math.acos(vec3(0, 0, 1).dot((vec3(...g_z_ccs))));
+    let z_angle= Math.atan2(g_z_ccs[2], g_z_ccs[0]) - Math.atan2(1, 0);
+    g_z_rot = z_angle;
+  }
+
+  display(context, graphics_state, dt = graphics_state.animation_delta_time / 1000)
+  {
+    const m = this.speed_multiplier * this.meters_per_frame;
+    const r = this.speed_multiplier * this.radians_per_frame;
+
+    if (this.will_take_over_graphics_state)
+    {
+      this.reset(graphics_state);
+      this.will_take_over_graphics_state = false;
+    }
+    if (!this.mouse_enabled_canvases.has(context.canvas))
+    {
+      this.add_mouse_controls(context.canvas);
+      this.mouse_enabled_canvases.add(context.canvas);
+    }
+
+    this.first_person_flyaround(dt * r, dt * m);
+
+    // We only want to move our view if we are locked into the canvas.
+    if (!this.mouse.anchor)
+      this.third_person_arcball(dt * r);
+
+    this.pos = this.inverse().times(vec4(0, 0, 0, 1));
+    this.z_axis = this.inverse().times(vec4(0, 0, 1, 0));
+  }
+};
+
 export class Shape_From_File extends Shape
 {                                   // **Shape_From_File** is a versatile standalone Shape that imports
                                     // all its arrays' data from an .obj 3D model file.
@@ -125,7 +256,7 @@ export class Project_Base extends Scene
 
       this.shapes.ground.arrays.texture_coord.forEach( p => p.scale_by(50));
       const phong = new defs.Phong_Shader();
-      const textured = new defs.Textured_Phong( 1 )
+      const textured = new defs.Textured_Phong( 1 );
       this.materials = { plastic: new Material( phong,
                                     { ambient: .2, diffusivity: 1, specularity: .5, color: color( .9,.5,.9,1 ) } ),
                         metal: new Material( phong,
@@ -138,8 +269,8 @@ export class Project_Base extends Scene
                         tree_trunk: new Material(phong, {ambient: .2, diffusivity: 1, specularity: .5, color: color(0.9, 0.4, 0.1, 1)}),
                         rock: new Material(phong, {ambient: .2, diffusivity: 1, specularity: 0.5, color: color(0.9, 0.9, 0.9, 1)})};
 
-      this.random_x = []
-      this.random_z = []
+      this.random_x = [];
+      this.random_z = [];
       for(var i = 0; i < 15; i+= 1){
         var R = 48 * Math.random();
         var theta = Math.random() * 2 * Math.PI;
@@ -158,6 +289,23 @@ export class Project_Base extends Scene
       this.key_triggered_button( "Hover dragonfly in place", [ "h" ], function() { this.hover ^= 1; } );
       this.new_line();
       this.key_triggered_button( "Swarm mode", [ "m" ], function() { this.swarm ^= 1; } );
+
+      this.new_line();
+      this.live_string(box => { box.textContent =
+          `Cam Looking At: (${g_cam_looking_at[0].toFixed(2)},
+          ${g_cam_looking_at[1].toFixed(2)}, ${g_cam_looking_at[2].toFixed(2)})`; });
+      this.new_line();
+      this.live_string( box => { box.textContent = `World Offset: (${g_origin_offset[0].toFixed(2)}, ${g_origin_offset[1].toFixed(2)}, ${g_origin_offset[2].toFixed(2)})`; });
+      this.new_line();
+      if (g_x_ccs.every(x => x !== NaN)) {
+        this.live_string( box => { box.textContent = `X CCS: (${g_x_ccs[0].toFixed(2)}, ${g_x_ccs[1].toFixed(2)}, ${g_x_ccs[2].toFixed(2)})`; });
+      }
+      this.new_line();
+      if (g_z_ccs.every(x => x !== NaN)) {
+        this.live_string( box => { box.textContent = `Z CCS: (${g_z_ccs[0].toFixed(2)}, ${g_z_ccs[1].toFixed(2)}, ${g_z_ccs[2].toFixed(2)})`; });
+      }
+      this.new_line();
+      this.live_string ( box => { box.textContent = `Z Angle: ${g_z_rot.toFixed(4)}`; });
     }
 
   display( context, program_state )
@@ -166,8 +314,10 @@ export class Project_Base extends Scene
       // We do it here instead of the constructor above so that we have access to context and program state
       if( !context.scratchpad.controls )
         {
-          this.children.push( context.scratchpad.controls = new defs.Movement_Controls() );
-          program_state.set_camera( Mat4.translation( 0,0,0 ) );
+          // this.children.push( context.scratchpad.controls = new defs.Movement_Controls() );
+          this.children.push(context.scratchpad.controls = new FPS_Controls());
+          // program_state.set_camera( Mat4.translation( 0,0,0 ) );
+          program_state.set_camera(Mat4.look_at(vec3(0, 0, 0), vec3(0, 0, 1), vec3(0, 1, 0)));
 
           // Get player location
           this.player_x = context.program_state.camera_inverse[0];
@@ -253,7 +403,6 @@ export class Project_Base extends Scene
   }
 }
 
-
 export class Project extends Project_Base
 {
   display( context, program_state )
@@ -287,7 +436,7 @@ export class Project extends Project_Base
       this.player_z = context.program_state.camera_inverse[2];
 
       // Ensure player cannot move in y-space
-      context.program_state.camera_inverse = Mat4.translation(this.player_x[3], 0, this.player_z[3]);
+      // context.program_state.camera_inverse = Mat4.translation(this.player_x[3], 0, this.player_z[3]);
 
       // Draw robot
       this.draw_robot(context, program_state, 0, this.robot_location[0]);
@@ -295,5 +444,9 @@ export class Project extends Project_Base
       this.draw_robot(context, program_state, 2, this.robot_location[2]);
       // Draw environment
       this.draw_environment(context, program_state, model_transform);
+
+      let cube_transform = Mat4.identity().times(Mat4.rotation(g_z_rot, 0, 1, 0)).times(Mat4.translation(0, 0, -5));
+      this.shapes.box.draw(context, program_state, cube_transform, this.materials.plastic);
+      // this.shapes.box.draw(context, program_state, Mat4.identity().times(Mat4.translation(3, 0, 0)), this.materials.plastic);
     }
 }
